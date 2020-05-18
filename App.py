e0a78f7b17fb8244f3f9ad0bcf2841a4c8655c40
fish895623 <dan990429@gmail.com>
2020-05-18 18:09:31 +0900

    test2

--- App.py
+++ App.py
@@ -2,21 +2,23 @@ import discord
 
 client = discord.Client()
 
-token_credential = open("credential.txt", 'r').readline()
+# NOTE Credential
+credential_token = open("credential.txt", 'r').readline()
+credential_riot = open('credential_riot.txt', 'r').readline()
 
 
 @client.event
 async def on_ready():
     print(client.user.id)
     print("ready")
-    game = discord.Game("집사")
+    game = discord.Game("집사 일")
     await client.change_presence(status=discord.Status.online, activity=game)
 
 
 @client.event
 async def on_message(message):
-    if message.content.startswith("!안녕"):
+    if message.content.startswith("!참가"):
         await message.channel.send("안녕하세요")
 
 
-client.run(token_credential)
+client.run(credential_token)
---
+++ LeagueOfLegend.py
@@ -0,0 +1,5 @@
+api_key = open('credential_riot.txt', 'r').readline()
+sohwan = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + \
+    'hide on bush' + '?api_key=' + api_key
+r = requests.get(sohwan)
+r.json()['id'
