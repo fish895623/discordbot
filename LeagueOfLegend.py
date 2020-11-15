f261222df470bde53b218d3386b93bd0612a41da
bsh <dan990429@gmail.com>
2020-11-15 17:35:35 +0900

    get league score

--- LeagueOfLegend.py
+++ LeagueOfLegend.py
@@ -1,5 +1,11 @@
-api_key = open('credential_riot.txt', 'r').readline()
-sohwan = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + \
-    'hide on bush' + '?api_key=' + api_key
+import requests
+
+api_key = open("credential_riot.txt", "r").readline()
+sohwan = (
+    "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/"
+    + "hide on bush"
+    + "?api_key="
+    + api_key
+)
 r = requests.get(sohwan)
-r.json()['id'
+r.json()["id"]
