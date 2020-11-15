8bbebffdb57019bd889daca0789908656c3bbf22
fish895623 <34264960+fish895623@users.noreply.github.com>
2020-11-15 20:22:36 +0900

    createChannel with commands

--- src/main/java/com/dan99/discord/events/HelloEvent.java
+++ src/main/java/com/dan99/discord/events/HelloEvent.java
@@ -6,18 +6,18 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class HelloEvent extends ListenerAdapter {
-    @Override
-    public void onMessageReceived(MessageReceivedEvent event) {
-        // String message = event.getMessage().getContentRaw();
-        if (event.getAuthor().isBot()) {
-            return;
-        }
-        Message message = event.getMessage();
-        String content = message.getContentRaw();
-        if (content.equals("!ping")) {
-            MessageChannel channel = event.getChannel();
-            channel.sendMessage("Pong!").queue(); // Important to call .queue() on the RestAction returned by
-                                                  // sendMessage(...)
-        }
+  @Override
+  public void onMessageReceived(MessageReceivedEvent event) {
+    // String message = event.getMessage().getContentRaw();
+    if (event.getAuthor().isBot()) {
+      return;
     }
+    Message message = event.getMessage();
+    String content = message.getContentRaw();
+    if (content.equals("!ping")) {
+      MessageChannel channel = event.getChannel();
+      channel.sendMessage("Pong!").queue();   // Important to call .queue() on the RestAction returned by
+      // sendMessage(...)
+    }
+  }
 }
