427ce7ec4adb711eaf569cfaf80b1c739add1151
fish895623 <34264960+fish895623@users.noreply.github.com>
2020-11-15 23:54:15 +0900

    Move dir and hide token

--- src/main/java/com/dan99/discord/Bot.java
+++ src/main/java/com/dan99/discord/Bot.java
@@ -1,13 +1,27 @@
 package com.dan99.discord;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+import com.dan99.discord.events.HelloEvent;
+import com.dan99.discord.events.text.createChannel;
+
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
-import com.dan99.discord.events.*;
-
 public class Bot {
     public static void main(String[] args) throws Exception {
-        JDA jda = JDABuilder.createDefault("").build();
+
+        String fileName = "token";
+
+        File file = new File(fileName);
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+        System.out.println(br.readLine());
+        br.close();
+        JDA jda = JDABuilder.createDefault(br.readLine()).build();
         jda.addEventListener(new HelloEvent());
+        jda.addEventListener(new createChannel());
     }
 }
